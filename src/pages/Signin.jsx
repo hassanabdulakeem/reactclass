c790799e57a394426d59d68e2bc5b380958e6f70
hassan <hassanabdulakeem48@gmail.com>
2025-05-22 11:06:10 +0100

    added verify

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -27,7 +27,7 @@ const Signin = () => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-        },
+        },  
         body: JSON.stringify(formData),
       });
 
